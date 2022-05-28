823fc5d2ba7afb8c6f2acbb3fe882c3aae9d1b0f
brunomdr <brunomdr46@gmail.com>
2022-05-28 16:16:46 -0300

    Criado componente de Login

--- frontend/src/App.js
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <h1>Glossário Libras</h1>;
-}
-
-export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import LoginForm from './LoginForm/index.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<LoginForm />} />
+      <Route path="/login" element={<LoginForm />} />
+    </Routes>
+  </BrowserRouter>
+);
