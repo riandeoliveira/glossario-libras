6842910a145ec2b3923064474822078448c4f71e
riandeoliveira <riandiasdeoliveira2001@gmail.com>
2022-06-14 18:46:52 -0300

    Limpeza de código

--- client/src/components/Button/index.jsx
+++ client/src/components/Button/index.jsx
@@ -1,14 +1,7 @@
 import styles from "./styles.module.css";
 
-export default function Button(props) {
-  // Se receber a prop submit o botão será de enviar, senão será um botão comum.
-
-  return (
-    <button
-      type={props.submit ? "submit" : "button"}
-      className={styles.button_container}
-    >
-      {props.children}
-    </button>
-  );
-}
+export const Button = ({ submit, children }) => (
+  <button type={submit ? "submit" : "button"} className={styles.button}>
+    {children}
+  </button>
+);
