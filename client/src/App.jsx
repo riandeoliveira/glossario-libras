46e62e23b995312dfa8367d89072fd47f7f6b146
riandeoliveira <riandiasdeoliveira2001@gmail.com>
2022-06-14 19:06:49 -0300

    Limpeza de código

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./components/Layout";
+import { Layout } from "./components/Layout";
 import { Login } from "./pages/Login";
 import { Register } from "./pages/Register";
 import Home from "./pages/Home";
