de30d7b9b6a00cdf8be1bcb26dc35492e7222c24
riandeoliveira <riandiasdeoliveira2001@gmail.com>
2022-06-15 01:09:25 -0300

    Formatado Home

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,12 +1,12 @@
-import Header from '../../components/Header/index.jsx';
-import TodasUnidades from '../../components/TodasUnidades/index.jsx';
-import { Helmet } from 'react-helmet';
-import './styles.css';
+import { Header } from "../../components/Header/index.jsx";
+import TodasUnidades from "../../components/TodasUnidades/index.jsx";
+import { Helmet } from "react-helmet";
+import "./styles.css";
 
 export default function Home() {
   return (
     <>
-    <Helmet>
+      <Helmet>
         <title>Glossário de LIBRAS | Home</title>
       </Helmet>
       <Header />
