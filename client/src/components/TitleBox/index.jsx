fd8cfb240f8d0ef0de922c372d8f7c42f5c5578d
riandeoliveira <riandiasdeoliveira2001@gmail.com>
2022-06-15 15:22:12 -0300

    Atualizado componente TitleBox

--- client/src/components/TitleBox/index.jsx
+++ client/src/components/TitleBox/index.jsx
@@ -3,7 +3,9 @@ import * as S from "./styles";
 // Caixa de título e subtítulo para início de páginas
 export const TitleBox = ({ title, subtitle }) => (
   <S.Container>
-    <h1>{title}</h1>
-    <p>{subtitle}</p>
+    <div>
+      <h1>{title}</h1>
+      <p>{subtitle}</p>
+    </div>
   </S.Container>
 );
--- client/src/components/TitleBox/styles.js
+++ client/src/components/TitleBox/styles.js
@@ -2,10 +2,17 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   display: flex;
-  flex-direction: column;
-  gap: 10px;
-  text-align: center;
-  margin: 20px 0;
+  justify-content: center;
+  padding-top: 20px;
+
+  div {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    justify-content: center;
+    text-align: center;
+    width: 1200px;
+  }
 
   h1,
   p {
