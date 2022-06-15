16d7a434db8fdaf76649910fdad4957be5c5433a
riandeoliveira <riandiasdeoliveira2001@gmail.com>
2022-06-15 16:26:33 -0300

    Removido códigos comentados

--- client/src/components/SignCard/index.jsx
+++ client/src/components/SignCard/index.jsx
@@ -9,10 +9,4 @@ export const SignCard = ({ image, letter }) => (
     />
     <h3>{letter}</h3>
   </S.Card>
-  // <Card style={{ width: "10rem" }}>
-  //   <Card.Img variant="top" src={props.image} />
-  //   <Card.Body>
-  //     <Card.Title>{props.letter}</Card.Title>
-  //   </Card.Body>
-  // </Card>
 );
--- client/src/components/StoryCard/index.jsx
+++ client/src/components/StoryCard/index.jsx
@@ -6,12 +6,4 @@ export const StoryCard = ({ image, title, exp }) => (
     <h3>{title}</h3>
     <span>+ {exp} EXP</span>
   </S.Card>
-  // <Card style={{ width: "10rem" }}>
-  //   <Card.Img variant="top" src={props.image} />
-  //   <Card.Body>
-  //     <Card.Title>{props.title}</Card.Title>
-  //     <Card.Text>{props.exp}</Card.Text>
-  //     <Button variant="primary">Jogar</Button>
-  //   </Card.Body>
-  // </Card>
 );
