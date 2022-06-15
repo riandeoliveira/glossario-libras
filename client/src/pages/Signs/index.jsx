ac2ae26c919742368225fef2921211ba8b2994b1
riandeoliveira <riandiasdeoliveira2001@gmail.com>
2022-06-15 15:19:52 -0300

    Reformulado página de sinais

--- client/src/pages/Signs/index.jsx
+++ client/src/pages/Signs/index.jsx
@@ -1,8 +1,9 @@
 import { Header } from "../../components/Header";
 import { Helmet } from "react-helmet";
 import { SignCardsArea } from "../../components/SignCardsArea";
-import "./styles.css";
 import { TitleBox } from "../../components/TitleBox";
+import { SectionTitle } from "../../components/SectionTitle";
+import * as S from "./styles";
 
 export const Signs = () => (
   <>
@@ -10,41 +11,15 @@ export const Signs = () => (
       <title>Glossário de LIBRAS | Sinais</title>
     </Helmet>
     <Header />
-    <TitleBox
-      title="Vamos aprender os sinais de LIBRAS"
-      subtitle="Aprenda os sinais mais usados na Língua Brasileira de Sinais"
-    />
-    <h2 className="niveis-historias">
-      <span>Alfabeto</span>
-    </h2>
-    <div className="conteudo-sinais">
-      <SignCardsArea />
-      {/* <SignCard title="A" image={"https://i.imgur.com/Ml2muS3.png"} /> */}
-      {/* <SignCard title="B" image={"https://i.imgur.com/ytosRaF.png"} /> */}
-      {/* <SignCard title="C" image={"https://i.imgur.com/UJKrNBu.png"} /> */}
-      {/* <SignCard title="D" image={"https://i.imgur.com/S5VqB9O.png"} /> */}
-      {/* <SignCard title="E" image={"https://i.imgur.com/URGtnHD.png"} /> */}
-      {/* <SignCard title="F" image={"https://i.imgur.com/ZjSQd20.png"} /> */}
-      {/* <SignCard title="G" image={"https://i.imgur.com/0nTVQ4k.png"} /> */}
-      {/* <SignCard title="H" image={"https://i.imgur.com/hJL2nhd.png"} /> */}
-      {/* <SignCard title="I" image={"https://i.imgur.com/na3XSqM.png"} /> */}
-      {/* <SignCard title="J" image={"https://i.imgur.com/EtVjcCJ.png"} /> */}
-      {/* <SignCard title="K" image={"https://i.imgur.com/5AIUrL5.png"} /> */}
-      {/* <SignCard title="L" image={"https://i.imgur.com/e7Qzx40.png"} /> */}
-      {/* <SignCard title="M" image={"https://i.imgur.com/IEFBPCG.png"} /> */}
-      {/* <SignCard title="N" image={"https://i.imgur.com/V1UKMob.png"} /> */}
-      {/* <SignCard title="O" image={"https://i.imgur.com/SfTuMZu.png"} /> */}
-      {/* <SignCard title="P" image={"https://i.imgur.com/ZBN96SH.png"} /> */}
-      {/* <SignCard title="Q" image={"https://i.imgur.com/DRBx1ad.png"} /> */}
-      {/* <SignCard title="R" image={"https://i.imgur.com/qglXcCf.png"} /> */}
-      {/* <SignCard title="S" image={"https://i.imgur.com/Ic4k40y.png"} /> */}
-      {/* <SignCard title="T" image={"https://i.imgur.com/Boms1iH.png"} /> */}
-      {/* <SignCard title="U" image={"https://i.imgur.com/4RDyr0x.png"} /> */}
-      {/* <SignCard title="V" image={"https://i.imgur.com/rx7siO9.png"} /> */}
-      {/* <SignCard title="W" image={"https://i.imgur.com/H5PWEX6.png"} /> */}
-      {/* <SignCard title="X" image={"https://i.imgur.com/UeAq31H.png"} /> */}
-      {/* <SignCard title="Y" image={"https://i.imgur.com/7cqyElt.png"} /> */}
-      {/* <SignCard title="Z" image={"https://i.imgur.com/KvIqgxZ.png"} /> */}
-    </div>
+    <S.Main>
+      <TitleBox
+        title="Vamos aprender os sinais de LIBRAS"
+        subtitle="Aprenda os sinais mais usados na Língua Brasileira de Sinais"
+      />
+      <section>
+        <SectionTitle title="Alfabeto" />
+        <SignCardsArea />
+      </section>
+    </S.Main>
   </>
 );
--- client/src/pages/Signs/styles.js
+++ client/src/pages/Signs/styles.js
@@ -1,1 +1,17 @@
 import styled from "styled-components";
+
+export const Main = styled.main`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  padding: 20px;
+  gap: 40px;
+
+  section {
+    display: flex;
+    flex-direction: column;
+    gap: 40px;
+  }
+`;
+
+export const Container = styled.div``;
