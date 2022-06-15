cccd62de90b149498cae451eebd99477c900230b
riandeoliveira <riandiasdeoliveira2001@gmail.com>
2022-06-15 15:19:30 -0300

    Reformulado página de histórias

--- client/src/pages/Stories/styles.js
+++ client/src/pages/Stories/styles.js
@@ -1,2 +1,15 @@
 import styled from "styled-components";
 
+export const Main = styled.main`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  padding: 20px;
+  gap: 40px;
+
+  section {
+    display: flex;
+    flex-direction: column;
+    gap: 40px;
+  }
+`;
