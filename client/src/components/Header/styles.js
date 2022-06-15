660a8f0d6d00153d0cf7afdcb5b59acebab0db04
riandeoliveira <riandiasdeoliveira2001@gmail.com>
2022-06-15 01:08:53 -0300

    Melhorado estilos do Header

--- client/src/components/Header/styles.js
+++ client/src/components/Header/styles.js
@@ -29,10 +29,25 @@ export const Header = styled.header`
           align-items: center;
           text-decoration: none;
           color: white;
+          transition: opacity 0.2s;
 
           svg {
             width: 24px;
             fill: white;
+          }
+
+          &:hover {
+            opacity: 0.5;
+          }
+        }
+
+        @media screen and (max-width: 720px) {
+          a {
+            padding: 20px;
+          }
+
+          span {
+            display: none;
           }
         }
       }
