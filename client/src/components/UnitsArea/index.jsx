11147c299f6c268ffbd9c5d81026d1e986d3fdf5
riandeoliveira <riandiasdeoliveira2001@gmail.com>
2022-06-18 16:33:54 -0300

    Adicionado key em cada unidade

--- client/src/components/UnitsArea/index.jsx
+++ client/src/components/UnitsArea/index.jsx
@@ -6,22 +6,22 @@ export const UnitsArea = ({ sectionName }) => {
   return (
     <div className={styles.container}>
       <div className={styles.easy_container}>
-        {units[sectionName].map(({ difficulty, image_url }) => (
+        {units[sectionName].map(({ difficulty, image_url }, i) => (
           <>
             {difficulty === "easy" && (
-              <Unity difficulty={difficulty} image={image_url} />
+              <Unity difficulty={difficulty} image={image_url} key={i} />
             )}
           </>
         ))}
       </div>
       <div className={styles.medium_hard_container}>
-        {units[sectionName].map(({ difficulty, image_url }) => (
+        {units[sectionName].map(({ difficulty, image_url }, i) => (
           <>
             {difficulty === "medium" && (
-              <Unity difficulty={difficulty} image={image_url} />
+              <Unity difficulty={difficulty} image={image_url} key={i} />
             )}
             {difficulty === "hard" && (
-              <Unity difficulty={difficulty} image={image_url} />
+              <Unity difficulty={difficulty} image={image_url} key={i} />
             )}
           </>
         ))}
