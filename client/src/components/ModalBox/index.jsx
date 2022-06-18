91924ba362cf4904a625db38680b54cbbfc1331c
riandeoliveira <riandiasdeoliveira2001@gmail.com>
2022-06-18 16:34:00 -0300

    Aprimorado ModalBox

--- client/src/components/ModalBox/index.jsx
+++ client/src/components/ModalBox/index.jsx
@@ -3,9 +3,18 @@ import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import { useContext } from "react";
 import { ModalContext } from "../../contexts/modal";
+import { Link } from "react-router-dom";
 
 export const ModalBox = ({ title, message }) => {
   const { show, handleClose } = useContext(ModalContext);
+
+  const checkPathname = () => {
+    const route = location.pathname;
+
+    if (route.includes("aprender")) return "/aprender";
+
+    return "/historias";
+  };
 
   return (
     <>
@@ -18,8 +27,10 @@ export const ModalBox = ({ title, message }) => {
           <Button variant="secondary" onClick={handleClose}>
             Continuar
           </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Sair
+          <Button variant="primary">
+            <Link to={checkPathname()} className={styles.link}>
+              Sair
+            </Link>
           </Button>
         </Modal.Footer>
       </Modal>
