ca1807ced6373070ee901c9b1d1ddcee52edad13
riandeoliveira <riandiasdeoliveira2001@gmail.com>
2022-06-19 14:23:08 -0300

    Aprimorado unidades de aprender

--- client/src/components/Unity/index.jsx
+++ client/src/components/Unity/index.jsx
@@ -1,7 +1,10 @@
 import styles from "./styles.module.scss";
 
 export const Unity = ({ difficulty, image }) => (
-  <div className={`${styles.container} ${styles[difficulty]}`}>
-    <img src={image} alt="" className={styles.image} />
+  <div className={styles.container}>
+    <div className={`${styles.unity} ${styles[difficulty]}`}>
+      <img src={image} alt="" className={styles.image} />
+    </div>
+    <span className={styles.title}>Nome da unidade</span>
   </div>
 );
