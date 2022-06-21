4e97eaa8180d0b63efe8d01c299b30fa9ab10af6
riandeoliveira <riandiasdeoliveira2001@gmail.com>
2022-06-21 15:02:48 -0300

    Documentado contextos

--- client/src/contexts/QuestionContext.jsx
+++ client/src/contexts/QuestionContext.jsx
@@ -1,17 +1,21 @@
 import { createContext, useState } from "react";
 
+// Cria um contexto para as questões.
 export const QuestionContext = createContext();
 
 export const QuestionProvider = ({ children }) => {
+  // Estados da questão.
   const [percentage, setPercentage] = useState(0);
   const [index, setIndex] = useState(0);
   const [isOpen, setIsOpen] = useState(false);
 
+  // Lidam com os estados.
   const handlePercentage = () => setPercentage(percentage + 20);
   const handleIndex = () => setIndex(index + 1);
   const openModal = () => setIsOpen(true);
   const closeModal = () => setIsOpen(false);
 
+  // Dados a serem exportados.
   const data = {
     percentage,
     index,
