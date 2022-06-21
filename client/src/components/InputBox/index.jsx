16554c5156bec19f6ac5aad9fe62b1dd4b34765d
riandeoliveira <riandiasdeoliveira2001@gmail.com>
2022-06-21 20:15:28 -0300

    Documentado componente InputBox

--- client/src/components/InputBox/index.jsx
+++ client/src/components/InputBox/index.jsx
@@ -1,6 +1,7 @@
 import styles from "./styles.module.scss";
 import { FormControl } from "react-bootstrap";
 
+// Caixa de input para formulários
 export const InputBox = ({ title, type, message }) => (
   <div className={styles.input_container}>
     <label className={styles.label}>{title}</label>
