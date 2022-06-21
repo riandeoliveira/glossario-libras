db3ee08a92af7e3a1806b1812602f2dbf4ad0442
riandeoliveira <riandiasdeoliveira2001@gmail.com>
2022-06-21 12:50:09 -0300

    Aprimorado página de atividade

--- client/src/pages/Test/index.jsx
+++ client/src/pages/Test/index.jsx
@@ -2,14 +2,53 @@ import styles from "./styles.module.scss";
 import { Helmet } from "react-helmet";
 import { ExerciseHeader } from "../../components/ExerciseHeader";
 import { ModalProvider } from "../../contexts/modal";
+import { GifCard } from "../../components/GifCard";
+import computador from "../../assets/gifs/computador.gif";
+import { useState } from "react";
+import { ExerciseFooter } from "../../components/ExerciseFooter";
 
-export const Test = () => (
-  <>
-    <Helmet>
-      <title>TESTE</title>
-    </Helmet>
-    <ModalProvider>
-      <ExerciseHeader />
-    </ModalProvider>
-  </>
-);
+export const Test = ({ data }) => {
+  const [index, setIndex] = useState(0);
+
+  const handleIndex = (answer, word) => {
+    if (answer) {
+      alert("Resposta Correta!");
+
+      setIndex(index + 1);
+    } else alert(`Resposta Errada! A opção selecionada significa: ${word}`);
+  };
+
+  return (
+    <>
+      <Helmet>
+        <title>Atividade | Glossário de LIBRAS</title>
+      </Helmet>
+      <ModalProvider>
+        <ExerciseHeader />
+      </ModalProvider>
+      <main className={styles.main}>
+        <section className={styles.container}>
+          <div className={styles.question_container}>
+            <div className={styles.question_content}>
+              <p className={styles.question}>
+                Traduza a palavra:{" "}
+                <span className={styles.word}>{data[index].word}</span>
+              </p>
+            </div>
+          </div>
+          <div className={styles.gifs_container}>
+            <div className={styles.gifs}>
+              {data[index].answers.map((card) => (
+                <GifCard
+                  gif={card.gif_path}
+                  onClick={() => handleIndex(card.right_answer, card.gif_word)}
+                />
+              ))}
+            </div>
+          </div>
+        </section>
+      </main>
+      <ExerciseFooter />
+    </>
+  );
+};
