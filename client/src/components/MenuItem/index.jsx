4c24dc583d229daf2389a84fbf8f34a4e843520d
riandeoliveira <riandiasdeoliveira2001@gmail.com>
2022-06-23 10:56:19 -0300

    Documentado componente MenuItem

--- client/src/components/MenuItem/index.jsx
+++ client/src/components/MenuItem/index.jsx
@@ -1,6 +1,7 @@
-import { Link } from "react-router-dom";
 import styles from "./styles.module.scss";
+import { Link } from "react-router-dom";
 
+// Item do cabeçalho
 export const MenuItem = ({ link, icon, title }) => (
   <div className={styles.menu_item}>
     <Link to={link} className={styles.link}>
