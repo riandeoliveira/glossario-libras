d346ba27ff0bef813f546dc4c207b6433e9e6935
riandeoliveira <riandiasdeoliveira2001@gmail.com>
2022-06-23 10:30:46 -0300

    Refatorado componente UnitsBox

--- client/src/components/UnitsBox/index.jsx
+++ client/src/components/UnitsBox/index.jsx
@@ -1,16 +1,15 @@
 import styles from "./styles.module.scss";
 import { UnitsArea } from "../UnitsArea";
 
-export const UnitsBox = ({ sectionName }) => {
-  return (
-    <div className={styles.container}>
-      <div>
-        <UnitsArea sectionName={sectionName} difficultyLevel="easy" />
-      </div>
-      <div className={styles.medium_hard_container}>
-        <UnitsArea sectionName={sectionName} difficultyLevel="medium" />
-        <UnitsArea sectionName={sectionName} difficultyLevel="hard" />
-      </div>
+// Caixa contendo a área de unidades
+export const UnitsBox = ({ sectionName }) => (
+  <div className={styles.container}>
+    <div>
+      <UnitsArea sectionName={sectionName} difficultyLevel="easy" />
     </div>
-  );
-};
+    <div className={styles.medium_hard_container}>
+      <UnitsArea sectionName={sectionName} difficultyLevel="medium" />
+      <UnitsArea sectionName={sectionName} difficultyLevel="hard" />
+    </div>
+  </div>
+);
