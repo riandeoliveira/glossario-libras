a3c26cc81e8f958b8ad9614d18373fede66bd54a
riandeoliveira <riandiasdeoliveira2001@gmail.com>
2022-06-23 11:17:59 -0300

    Refatorado componente SignCard

--- client/src/components/SignCard/index.jsx
+++ client/src/components/SignCard/index.jsx
@@ -1,11 +1,9 @@
 import styles from "./styles.module.scss";
 
+// Card de sinais da página Sinais
 export const SignCard = ({ image, title }) => (
   <div className={styles.card}>
-    <img
-      src={image}
-      alt="Letra do alfabeto na Linguagem Brasileira de Sinais"
-    />
+    <img src={image} alt="Letra do alfabeto da Língua Brasileira de Sinais" />
     <h3 className={styles.title}>{title}</h3>
   </div>
 );
